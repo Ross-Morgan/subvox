38b2ae8a909c0a1259dcf0af153f457c47cac97e
Ross Morgan <morgan.ross.512@gmail.com>
2026-07-16 15:55:21 +0100

    add parallelisation benchmark (broken rn)

--- subvox-backend/benches/stft_bench.rs
+++ subvox-backend/benches/stft_bench.rs
@@ -1,14 +1,12 @@
-use std::hint::black_box;
+use std::{hint::black_box, sync::Arc};
 
 use criterion::{Criterion, criterion_group, criterion_main};
 use subvox_backend::{FourierTransformer, load_audio_file};
 
 fn bench_stft(c: &mut Criterion) {
-    let file = load_audio_file("../assets/vocal-1.wav").unwrap();
+    let file = load_audio_file("../assets/growl.wav").unwrap();
     let frames = file.to_f32_vec();
     let mut transformer = FourierTransformer::new();
-
-    // TODO: Warm up plan cache?
 
     let mut group = c.benchmark_group("stft");
 
@@ -25,5 +23,32 @@ fn bench_stft(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, bench_stft);
+fn bench_par_stft(c: &mut Criterion) {
+    let file = load_audio_file("../assets/growl.wav").unwrap();
+    let frames = file.to_f32_vec();
+    let mut planner = realfft::RealFftPlanner::<f32>::new();
+    let mut group = c.benchmark_group("stft");
+
+    group.sampling_mode(criterion::SamplingMode::Flat);
+
+    for window_size in (10..16).map(|i| 2usize.pow(i)).into_iter() {
+        let r2c = planner.plan_fft_forward(window_size);
+        for hop_size in [window_size / 4, window_size / 2, window_size] {
+            group.bench_function(format!("rayon {window_size}_window_{hop_size}_hop"), |b| {
+                b.iter(|| {
+                    subvox_backend::par_stft(
+                        black_box(&frames),
+                        window_size,
+                        hop_size,
+                        Arc::clone(&r2c),
+                    )
+                })
+            });
+        }
+    }
+
+    group.finish();
+}
+
+criterion_group!(benches, bench_stft, bench_par_stft);
 criterion_main!(benches);
